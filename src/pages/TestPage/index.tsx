e25b227b3bc2a53aebb6898d6899ac1079fcaee7
alexeyDevel <alexeydevelopermm@gmail.com>
2023-11-13 18:26:06 +0300

    add normalize and Tailwind CSS

---
+++ src/pages/TestPage/index.tsx
@@ -0,0 +1,11 @@
+import style from "./style.module.css";
+
+function TestPage() {
+  return (
+    <div>
+        <h1 className={style.title}>Тестовая страница</h1>
+    </div>
+  );
+}
+
+export default TestPage;
