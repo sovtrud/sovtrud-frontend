8f24de4f42b3638e41093b43cfa38cb0df53f2d5
Marina Shopina <marinashopina95@gmail.com>
2023-11-24 23:44:41 +0300

    start customize input

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,26 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import TestPage from "@/pages/TestPage";
 import "./styles/App.scss";
+import { ConfigProvider } from "antd";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<TestPage />}></Route>
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+    <ConfigProvider
+      theme={{
+        components: {
+          Input: {
+            colorPrimary: "#FFAE40",
+            colorBgContainer: "#F8F8F8",
+            borderRadius: 8,
+          },
+        },
+      }}
+    >
+      <Routes>
+        <Route path="/" element={<TestPage />}></Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </ConfigProvider>
   );
 }
 
