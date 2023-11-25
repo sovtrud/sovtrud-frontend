3aea9e5934b1a5f1b2ad8f03f4490c72b9efcd7b
Anna <thoria@yandex.ru>
2023-11-25 09:55:27 +0300

    partly add hovers to ButtonWithText

--- src/shared/ui/buttons/ButtonWithText/index.tsx
+++ src/shared/ui/buttons/ButtonWithText/index.tsx
@@ -1,3 +1,4 @@
+import style from "./styles.module.scss";
 import { Button as AntdButton } from "antd";
 
 type TButtonSize = {
@@ -34,19 +35,41 @@ export default function ButtonWithText(props: ButtonWithTextProps) {
     }
   }
 
+  function setHoverBackgroundColor(type: string) {
+    if (type === "accent") {
+      return "var(--color-dark-yellow-primary)";
+    } else {
+      return "var(--color-light-brown)";
+    }
+  }
+
+  function setBorder(type: string, size: string) {
+    if (type === "accent" && size === "s") {
+      return "1px solid var(--color-yellow-primary)";
+    } else {
+      return "none";
+    }
+  }
+
+  function setHoverBorder(type: string, size: string) {
+    if (type === "accent" || size === "s") {
+      return "none";
+    } else {
+      return "1px solid var(--color-brown)";
+    }
+  }
+
   return (
     <AntdButton
       type="primary"
+      className={style.button}
       style={{
         backgroundColor: setBackgroundColor(props.type, props.size.size),
         color: setTextColor(props.type, props.size.size),
         minWidth: props.size.minWidth,
         minHeight: props.size.minHeight,
         borderRadius: "10px",
-        border:
-          props.type === "primary" && props.size.size === "s"
-            ? "1px solid var(--color-yellow-primary)"
-            : "none",
+        border: setBorder(props.type, props.size.size),
         fontSize: props.size.fontSize,
         fontWeight: props.size.fontWeight,
         lineHeight: props.size.lineHeight,
@@ -56,6 +79,20 @@ export default function ButtonWithText(props: ButtonWithTextProps) {
         justifyContent: "center",
         alignItems: "center",
         boxShadow: "none",
+      }}
+      onMouseOver={(e) => {
+        e.currentTarget.style.background = setHoverBackgroundColor(props.type);
+        e.currentTarget.style.border = setHoverBorder(
+          props.type,
+          props.size.size
+        );
+      }}
+      onMouseOut={(e) => {
+        e.currentTarget.style.background = setBackgroundColor(
+          props.type,
+          props.size.size
+        );
+        e.currentTarget.style.border = setBorder(props.type, props.size.size);
       }}
     >
       {props.children}
