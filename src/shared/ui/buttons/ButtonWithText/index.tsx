eb262f46d0f46d837e6be050983640571351697f
Anna <thoria@yandex.ru>
2023-11-25 13:47:20 +0300

    fix className in ButtonWithText

--- src/shared/ui/buttons/ButtonWithText/index.tsx
+++ src/shared/ui/buttons/ButtonWithText/index.tsx
@@ -12,11 +12,8 @@ export default function ButtonWithText(props: ButtonWithTextProps) {
     <AntdButton
       type="primary"
       className={`button 
-      ${props.type === "accent" ? "button_type_accent" : "button_type_primary"}
-      ${props.size === "s" ? "button_size_s" : ""}
-      ${props.size === "m" ? "button_size_m" : ""}
-      ${props.size === "l" ? "button_size_l" : ""}
-      ${props.size === "xl" ? "button_size_xl" : ""}`}
+      ${props.type === "accent" ? "button_type_accent" : "button_type_primary"} 
+      button_size_${props.size}`}
     >
       {props.children}
     </AntdButton>
