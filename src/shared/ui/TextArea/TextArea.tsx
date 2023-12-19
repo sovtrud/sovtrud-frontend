67c8f0afffb2130962d5cb1adcac2b12e0978314
Marina Shopina <marinashopina95@gmail.com>
2023-12-19 21:52:38 +0300

    text area buttons

--- src/shared/ui/TextArea/TextArea.tsx
+++ src/shared/ui/TextArea/TextArea.tsx
@@ -1,15 +1,44 @@
 import { Input as AntdInput } from "antd";
 import "./styles.scss";
-import { TextAreaProps } from "antd/es/input";
+import { TextAreaProps as AntdTextAreaProps } from "antd/es/input";
+import IconWithSvg from "../icons/IconWithSvg";
+import Close from "@/assets/icons/close.svg?react";
+import Check from "@/assets/icons/check.svg?react";
+import Button from "@/shared/ui/buttons/Button";
+import { SvgSize } from "@/shared/ui/icons/IconWithSvg";
+import { useState } from "react";
 
-const { TextArea: TextArea1 } = AntdInput;
+const { TextArea: AntdTextArea } = AntdInput;
+export type TextAreaProps = AntdTextAreaProps & {
+  onSave?: (text: string) => void;
+};
 
-export default function TextArea({ className, ...rest }: TextAreaProps) {
+export default function TextArea({
+  className,
+  onSave,
+  ...rest
+}: TextAreaProps) {
+  const [text, setText] = useState<string>("");
+  function handleClickClose() {
+    setText("");
+  }
+  function handleClickSave() {
+    onSave?.(text);
+  }
   return (
-    <TextArea1
-      className={`${className}`}
-      autoSize={{ minRows: 3, maxRows: 6 }}
-      {...rest}
-    />
+    <div className={`${className} text_area_container`}>
+      <AntdTextArea
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        autoSize={{ minRows: 3, maxRows: 6 }}
+        {...rest}
+      />
+      <Button className="text-area_btn_close" onClick={handleClickClose}>
+        <IconWithSvg children={Close} status="active" size={SvgSize.BASE16} />
+      </Button>
+      <Button className="text-area_btn_check" onClick={handleClickSave}>
+        <IconWithSvg children={Check} status="active" size={SvgSize.BASE16} />
+      </Button>
+    </div>
   );
 }
