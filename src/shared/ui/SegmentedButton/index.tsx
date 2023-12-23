ab1070566b4751f081d13659eb16707a37514d89
Anna <thoria@yandex.ru>
2023-12-23 04:52:31 +0300

    fix SegmentedButton styles

--- src/shared/ui/SegmentedButton/index.tsx
+++ src/shared/ui/SegmentedButton/index.tsx
@@ -2,5 +2,5 @@ import { Segmented as AntdSegmented } from "antd";
 import './styles.scss';
 
 export default function SegmentedButton() {
-  return <AntdSegmented options={['Плановые', 'Срочные', 'Просроченные']} size="large" ></AntdSegmented>;
+  return <AntdSegmented options={['Плановые', 'Срочные', 'Просроченные']} size="large" className="segmented-button" ></AntdSegmented>;
 }
